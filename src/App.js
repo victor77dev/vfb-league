6c0b7194fefff712dd5f525bf0535797d0428e3e
victor77dev <victor77dev@gmail.com>
2022-09-23 21:22:04 +0200

    Add layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import ThemeProvider from 'react-bootstrap/ThemeProvider'
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import {Entrance} from './features/entrance';
 
 function App() {
   return (
-    <Entrance />
+    <ThemeProvider
+      breakpoints={['xxxl', 'xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs']}
+      minBreakpoint="xxs"
+    >
+      <Entrance />
+    </ThemeProvider>
   );
 }
 
