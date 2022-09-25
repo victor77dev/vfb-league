36fb9f9576fbda7fecb5947f94bec511ce5b4338
victor77dev <victor77dev@gmail.com>
2022-09-25 21:12:10 +0000

    Build from main 789757c2e87f59d58684054308fdd4c809cec6ef

--- src/components/filter.js
+++
@@ -1,9 +0,0 @@
-import ButtonGroup from 'react-bootstrap/ButtonGroup';
-
-export const Filter = ({children}) => {
-    return (
-        <ButtonGroup className="mb-2">
-            {children}
-        </ButtonGroup>
-    );
-}
