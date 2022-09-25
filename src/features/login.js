70fb915cb1e41edab4541269778ba5b46e890bbf
victor77dev <victor77dev@gmail.com>
2022-09-25 20:56:50 +0000

    Build from main 5dcfa7ac2be0b74595c4e2c666b0868d59263bdf

---
+++ src/features/login.js
@@ -0,0 +1,59 @@
+import {useState} from 'react';
+import Spinner from 'react-bootstrap/Spinner';
+
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+import {supabase} from './supabaseClient';
+
+export const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [loading, setLoading] = useState(false);
+    const [validated, setValidated] = useState(false);
+
+    const handleSignup = async (e) => {
+        e.preventDefault()
+        try {
+            setLoading(true)
+            const {error} = await supabase.auth.signInWithPassword({
+                email,
+                password,
+            });
+
+            if (error) throw error
+        } catch (error) {
+            alert(error.error_description || error.message)
+            setValidated(false);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return (
+        <Form validated={validated} onSubmit={handleSignup}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control
+                    type="email"
+                    placeholder="Enter email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+            </Form.Group>
+  
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+                {loading ? <Spinner animation="border" /> : 'Login'}
+            </Button>
+        </Form>
+    );
+}
