ef944f5176ed99803f4f4e7182f9a1f822c9189b
Victor Wu <victor.wu@zattoo.com>
2022-10-11 19:51:00 +0200

    Add official link

--- src/features/home.js
+++ src/features/home.js
@@ -6,6 +6,9 @@ import {Matches} from './matches';
 export const Home = ({team, matches, setFilteredTeam}) => {
     return (
         <>
+            <div>
+                <a href="https://bvbb-badminton.liga.nu/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/clubMeetings?club=18281"><b>Official site for match schedule</b></a>
+            </div>
             <Filter>
                 <TeamFilter team={team} setFilteredTeam={setFilteredTeam} />
             </Filter>
