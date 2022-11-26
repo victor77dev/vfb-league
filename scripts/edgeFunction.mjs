cd9f77f038ccc787b6d51cec62894f5031bcf8e1
Victor Wu <victor.wu@zattoo.com>
2022-11-26 16:13:28 +0100

    Add supabase link

--- scripts/edgeFunction.mjs
+++ scripts/edgeFunction.mjs
@@ -1,8 +1,21 @@
 import { exec } from 'child_process';
 
-// console.log(process.env.)
-
 const supabaseCli = 'node_modules/supabase/bin/supabase';
+const link = 'link';
+const password = `-p  ${process.env.SUPABASE_PROJECT_DB_KEY}`;
+const project = `--project-ref ${process.env.SUPABASE_PROJECT_REF}`;
+
+exec(`${supabaseCli} ${link} ${password} ${project}`, (error, stdout, stderr) => {
+  if (error || stderr) {
+    console.error(error);
+    console.log(stderr);
+    console.log(stdout);
+    process.exit(1);
+  }
+
+  console.log(stdout);
+});
+
 const setSecret = 'secrets set';
 const clientSecret = `REACT_APP_YOUTUBE_CLIENT_SECRET=${process.env.REACT_APP_YOUTUBE_CLIENT_SECRET}`;
 
