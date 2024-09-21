07932d01a82826ba6d60f869352873f9d57c6007
Victor Wu <victor77dev@gmail.com>
2024-09-21 14:34:23 +0200

    Update style

--- src/components/filter.js
+++ src/components/filter.js
@@ -2,7 +2,7 @@ import ButtonGroup from 'react-bootstrap/ButtonGroup';
 
 export const Filter = ({children}) => {
     return (
-        <ButtonGroup className="mb-2">
+        <ButtonGroup className="mb-2" style={{flexWrap: 'wrap'}}>
             {children}
         </ButtonGroup>
     );
